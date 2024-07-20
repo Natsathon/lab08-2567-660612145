9981fe2b08d5bcfba0bd62bea5225623f9df5eea
Apicha660612162 <apichalerdjintanakid@gmail.com>
2024-07-20 16:56:05 +0700

    gg

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer(props) {
+  return (
+    <div className="mt-3">
+    <p className="text-secondary text-center">
+      {props.year}{props.name} {props.studentld}
+    </p>
+  </div>
+  )
+}
