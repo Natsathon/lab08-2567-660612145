d48b591ffdf9b0842e7f7f532bd1a7e9bf7494a2
_pooh <natsathon2018@gmail.com>
2024-07-20 19:43:17 +0700

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -30,7 +30,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2024" fullname = "Kachapat Punthong" studentId = "660612137"/>
+      <Footer year="2024" fullname = "Natsathon" studentId = "660612145"/>
     </div>
   );
 }
